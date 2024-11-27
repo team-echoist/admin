c1c5daec3ea8e80596f4fff9506c3400a2ff8f14
a-honey <jungahyeon0512@khu.ac.kr>
2024-11-28 00:41:06 +0900

    :package: Chore: api 응답시 토큰 교체 및 401 stauts 시 삭제 -> loader가 페이지 이동

--- src/api/AxiosInstance.tsx
+++ src/api/AxiosInstance.tsx
@@ -18,4 +18,22 @@ AxiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    const newAccessToken = response.headers["x-access-token"];
+    if (newAccessToken) {
+      localStorage.removeItem("accessToken");
+      localStorage.setItem("accessToken", newAccessToken);
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("refreshToken");
+      localStorage.removeItem("accessToken");
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default AxiosInstance;
