997b1850334ba0e77582705aab2952070c96a4e1
Ahyeon, Jung <75254185+a-honey@users.noreply.github.com>
2024-12-01 07:17:51 +0900

    🔨 Refactor: 초기 렌더링 줄이기 (#10)
    * fix: Pretandard 로컬 폰트로 변경
    
    * refactor: Auth 로고 이미지 webp 변환
    
    * refactor: 이미지 preload 적용
    
    * fix: 로컬폰트 파일명 변경

--- src/pages/auth/_components/FormContainer.tsx
+++ src/pages/auth/_components/FormContainer.tsx
@@ -1,8 +1,10 @@
-import { FormHTMLAttributes, PropsWithChildren } from "react";
+import { FormHTMLAttributes, PropsWithChildren, useEffect } from "react";
+import { PRELOADED_IMAGES, preloadImage } from "../../../lib/preloadImage";
 
 import { Button } from "../../../components/ui/button";
 import { cn } from "../../../lib/utils";
-import logo from "../../../assets/black_logo.png";
+import logoPNG from "../../../assets/black_logo.png";
+import logoWEBP from "../../../assets/black_logo.webp";
 
 type FormContainerProps = PropsWithChildren<{
   move?: { label: string; to: string };
@@ -10,6 +12,14 @@ type FormContainerProps = PropsWithChildren<{
   FormHTMLAttributes<HTMLFormElement>;
 
 const FormContainer = ({ children, onSubmit, move }: FormContainerProps) => {
+  useEffect(() => {
+    preloadImage([logoWEBP, logoPNG]);
+
+    return () => {
+      PRELOADED_IMAGES.length = 0;
+    };
+  }, []);
+
   return (
     <form
       onSubmit={onSubmit}
@@ -19,9 +29,11 @@ const FormContainer = ({ children, onSubmit, move }: FormContainerProps) => {
       )}
     >
       <div className="rounded-[20px] overflow-hidden">
-        <img src={logo} alt="링크드아웃 로고" width={150} />
+        <picture>
+          <source srcSet={logoWEBP} type="image/webp" />
+          <img src={logoPNG} alt="링크드아웃 로고" width={150} height={150} />
+        </picture>
       </div>
-
       {children}
       <Button type="submit" className="w-full">
         제출
