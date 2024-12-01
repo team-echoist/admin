3029785d85bf92dbbdb206b2c826771bb883750b
a-honey <jungahyeon0512@khu.ac.kr>
2024-12-01 22:43:38 +0900

    refactor: authCheckLoader 함수 리팩토링

--- src/layouts/authCheckLoader.tsx
+++ src/layouts/authCheckLoader.tsx
@@ -9,48 +9,33 @@ type LoaderContext = "AuthLayout" | "DefaultLayout";
 export default async function authCheckLoader(context: LoaderContext) {
   const accessToken = localStorage.getItem("accessToken");
 
-  if (context === "AuthLayout") {
-    if (accessToken) {
-      try {
-        const isAlreadyRedirected = sessionStorage.getItem("redirected");
-        if (isAlreadyRedirected) {
-          return null;
-        }
+  const handleUnauthorized = () => {
+    localStorage.removeItem("accessToken");
+    return redirect(`/auth/${AuthPaths.LOGIN}`);
+  };
 
-        const response = await getUserPermission();
-        sessionStorage.setItem("redirected", "true");
+  if (!accessToken) {
+    return context === "DefaultLayout" ? handleUnauthorized() : null;
+  }
 
-        if (response && response.id !== undefined) {
-          localStorage.setItem("isRootAccount", `${response.id === 0}`);
-          return redirect(`/${DefaultPaths.DASHBOARD}`);
-        }
-      } catch (e) {
-        const error = e as ResponseErrorType;
-        if (error.status === 401) {
-          localStorage.removeItem("accessToken");
-          return redirect(`/auth/${AuthPaths.LOGIN}`);
-        }
-      }
-    } else {
+  try {
+    if (sessionStorage.getItem("redirected")) {
       return null;
     }
-  } else if (context === "DefaultLayout") {
-    if (!accessToken) {
-      return redirect(`/auth/${AuthPaths.LOGIN}`);
-    }
 
-    try {
-      const response = await getUserPermission();
-      if (response && response.id !== undefined) {
-        localStorage.setItem("isRootAccount", `${response.id === 0}`);
-        return null;
+    const response = await getUserPermission();
+    sessionStorage.setItem("redirected", "true");
+
+    if (response?.id !== undefined) {
+      localStorage.setItem("isRootAccount", `${response.id === 0}`);
+      if (context === "AuthLayout") {
+        return redirect(`/${DefaultPaths.DASHBOARD}`);
       }
-    } catch (e) {
-      const error = e as ResponseErrorType;
-      if (error.status === 401) {
-        localStorage.removeItem("accessToken");
-        return redirect(`/auth/${AuthPaths.LOGIN}`);
-      }
+    }
+  } catch (e) {
+    const error = e as ResponseErrorType;
+    if (error.status === 401) {
+      return handleUnauthorized();
     }
   }
 
