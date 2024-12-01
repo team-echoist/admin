513887eb26e9d483bbe8de7920bf5fcdbfa1285a
Ahyeon, Jung <75254185+a-honey@users.noreply.github.com>
2024-12-02 02:18:46 +0900

    ✨ Feat: 스토리북 chromatic 설치 및 fallback UI 변경 (#19)
    * feat: typography 스토리북 추가
    
    * feat: color 스토리북 추가
    
    * chore: 스토리북 제목 소문자로 변경
    
    * install: chromatic 설치
    
    * chore: 스토리북 파일 GNB와 Fallback 합쳐서 components로 분류
    
    * style: fallback UI 변경

--- src/components/fallback/LoadingFallback/index.tsx
+++ src/components/fallback/LoadingFallback/index.tsx
@@ -1,3 +1,9 @@
 export default function LoadingFallback() {
-  return <div>로딩중</div>;
+  return (
+    <div className="flex justify-center items-center bg-gray-100">
+      <div className="p-6 bg-white rounded-lg text-center text-xl">
+        로딩중입니다
+      </div>
+    </div>
+  );
 }
