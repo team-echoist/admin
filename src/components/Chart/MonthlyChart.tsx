bae95af89eed9891e81fecb45de23b68925bc46a
a-honey <jungahyeon0512@khu.ac.kr>
2024-12-04 22:58:19 +0900

    feat: Chart 컴포넌트 구현

--- src/components/Chart/MonthlyChart.tsx
+++ src/components/Chart/MonthlyChart.tsx
@@ -1,4 +1,4 @@
-import Chart from "react-apexcharts";
+import Chart from ".";
 
 const MonthlyChart = ({ data }: { data: { [key: string]: number } }) => {
   const today = new Date();
@@ -8,49 +8,12 @@ const MonthlyChart = ({ data }: { data: { [key: string]: number } }) => {
   const formattedData = Object.keys(data).map((day) => {
     const dayPadded = day.padStart(2, "0");
     return {
-      day: `${year}-${month.toString().padStart(2, "0")}-${dayPadded}`,
+      label: `${year}-${month.toString().padStart(2, "0")}-${dayPadded}`,
       value: data[day],
     };
   });
 
-  const categories = formattedData.map((item) => item.day);
-  const seriesData = formattedData.map((item) => item.value);
-
-  return (
-    <Chart
-      options={{
-        chart: {
-          id: "basic-bar",
-          toolbar: { show: false },
-        },
-        xaxis: {
-          categories: categories,
-          labels: {
-            show: false,
-          },
-        },
-        yaxis: {
-          labels: {
-            show: false,
-          },
-        },
-        tooltip: {
-          enabled: true,
-          x: {
-            format: "dd MMM",
-          },
-        },
-      }}
-      series={[
-        {
-          name: "카운트",
-          data: seriesData,
-        },
-      ]}
-      type="line"
-      width="400"
-    />
-  );
+  return <Chart data={formattedData} />;
 };
 
 export default MonthlyChart;
--- src/components/Chart/YearChart.tsx
+++ src/components/Chart/YearChart.tsx
@@ -1,4 +1,4 @@
-import Chart from "react-apexcharts";
+import Chart from ".";
 
 const YearChart = ({ data }: { data: { [key: string]: number } }) => {
   const today = new Date();
@@ -6,49 +6,12 @@ const YearChart = ({ data }: { data: { [key: string]: number } }) => {
 
   const formattedData = Object.keys(data).map((month) => {
     return {
-      day: `${year}-${month}`,
+      label: `${year}-${month}`,
       value: data[month],
     };
   });
 
-  const categories = formattedData.map((item) => item.day);
-  const seriesData = formattedData.map((item) => item.value);
-
-  return (
-    <Chart
-      options={{
-        chart: {
-          id: "basic-bar",
-          toolbar: { show: false },
-        },
-        xaxis: {
-          categories: categories,
-          labels: {
-            show: false,
-          },
-        },
-        yaxis: {
-          labels: {
-            show: false,
-          },
-        },
-        tooltip: {
-          enabled: true,
-          x: {
-            format: "yy MMM",
-          },
-        },
-      }}
-      series={[
-        {
-          name: "카운트",
-          data: seriesData,
-        },
-      ]}
-      type="line"
-      width="400"
-    />
-  );
+  return <Chart data={formattedData} />;
 };
 
 export default YearChart;
