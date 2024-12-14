90187c6243ca6eafa1f10d03e58d08912052b001
a-honey <jungahyeon0512@khu.ac.kr>
2024-12-15 03:54:41 +0900

    test: 관리자 테스트 코드 추가

---
+++ __tests__/manager.spec.ts
@@ -0,0 +1,49 @@
+import { expect, test } from "@chromatic-com/playwright";
+import {
+  managerHistoryListMock,
+  managerListMock,
+} from "../__mocks__/managerMock";
+
+test.describe("관리자 목록 페이지", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.addInitScript(() => {
+      localStorage.setItem("accessToken", "token");
+    });
+    await page.goto("http://localhost:5173/managers");
+
+    await page.route(managerListMock.url, (route) =>
+      route.fulfill(managerListMock.apiResponse)
+    );
+  });
+
+  test("관리자 목록 페이지를 보여준다.", async ({ page }) => {
+    await expect(page.getByText("총 관리자 수 6")).toBeVisible();
+  });
+
+  test("관리자를 클릭하면 에세이 해당 관리자의 페이지로 이동한다", async ({
+    page,
+  }) => {
+    await page.click('div:has-text("정아현")');
+
+    await page.goto("http://localhost:5173/managers/2");
+
+    await expect(page).toHaveURL("http://localhost:5173/managers/2");
+  });
+});
+
+test.describe("관리자 로그 리스트 페이지", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.addInitScript(() => {
+      localStorage.setItem("accessToken", "token");
+    });
+    await page.goto("http://localhost:5173/manager-history");
+
+    await page.route(managerHistoryListMock.url, (route) =>
+      route.fulfill(managerHistoryListMock.apiResponse)
+    );
+  });
+
+  test("관리자 로그 리스트 페이지를 보여준다.", async ({ page }) => {
+    await expect(page.getByText("활동 내용")).toBeVisible();
+  });
+});
