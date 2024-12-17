b8fa98934053be1d6bc5533ccc5150b495548e1f
Ahyeon, Jung <75254185+a-honey@users.noreply.github.com>
2024-12-17 16:53:19 +0900

    ✨ Feat: PWA 설치 (#47)
    * feat: vite-plugin-pwa 설치 및 아이콘 추가
    
    * fix: 임시 visualizer 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,37 @@
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "LinkedOut Admin",
+        short_name: "LinkedOut",
+        description: "LinkedOut Admin Page",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,13 +39,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      plugins: [
-        visualizer({
-          open: true,
-          filename: "bundle-visualizer.html",
-          template: "treemap",
-        }),
-      ],
+      plugins: [],
     },
   },
 });
